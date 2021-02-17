40df6611844d2f63625eb44f965ff4cdb595c2dd
ewhdtls0 <46704442+ewhdtls0@users.noreply.github.com>
2021-02-17 11:43:25 +0900

    Saving

--- frontend/src/Components/Reservation/ReservationView.js
+++ frontend/src/Components/Reservation/ReservationView.js
@@ -7,12 +7,27 @@ class ReservationView extends Component{
         return(
             <div>
                 <div className="Reservation_header">
-                    
+                    <h1 className="frame">
+                        <a className="products" href="/hotel">
+                            <div>
+                                <span>호텔<span style={{fontSize: "15px"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▼</span></span>
+                                <div className="productsDropDown">
+                                    <a className="productsDropDownMenu"><span>모텔</span></a>
+                                    <a className="productsDropDownMenu"><span>펜션</span></a>
+                                    <a className="productsDropDownMenu"><span>리조트</span></a>
+                                    <a className="productsDropDownMenu"><span>게스트하우스</span></a>
+                                    <a className="productsDropDownMenu"><span>한옥</span></a>
+                                    <a className="productsDropDownMenu"><span>캠핑</span></a>
+                                </div>
+                            </div>
+
+                        </a>
+                    </h1>
                 </div>
                 <Grid container style={{top: "260px"}}>
-                    <Grid item xs={2} style={{background: "red"}}>레드</Grid>
-                    <Grid item xs={8} style={{background: "blue"}}>블루</Grid>
-                    <Grid item xs={2} style={{background: "green"}}>그린</Grid>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={8}></Grid>
+                    <Grid item xs={2}></Grid>
                 </Grid>
             </div>
         )
