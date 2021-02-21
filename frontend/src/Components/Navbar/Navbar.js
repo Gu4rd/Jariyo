e751c68ac40a8242bb58839692fce184f1f16a73
gu4rd <rhrudgh12@Naver.com>
2021-02-21 13:40:21 +0900

    네비바 색변환 추가

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -5,8 +5,16 @@ import './Navbar.css';
 
 class NavBar extends Component {
 
-  state = { clicked: false }
+  state = { clicked: false,
+            scrolled: false};
 
+  changeBackground = () => {
+    if(window.scrollY >= 80){
+      this.setState({ scrolled : true})
+    } else {
+      this.setState({ scrolled : false})
+    }
+  }
   handleClick = () => {
     this.setState({ clicked : !this.state.clicked})
   }
@@ -19,37 +27,40 @@ class NavBar extends Component {
   }
 
   render() {
+    window.addEventListener('scroll', this.changeBackground);
     return (
-      <nav className="NavbarItems">
-        <a href="/"><h1 className="navbar-logo"><i className="fab fa-react"></i>Jariyo</h1></a>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-        </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index} onMouseOut={this.moreView2}>
-                {item.title == '더 보기'
-                ? <a className={item.cName} href={item.url} onMouseOver={this.moreView}>
-                    {item.title}
-                  </a>
-                : <a className={item.cName} href={item.url}>
-                    {item.title}
-                  </a>
-                }
-                {item.title == '더 보기' &&
-                  <ul className="more" onMouseOver={this.moreView}>
-                  {item.more.title.map((a, idx) =>
-                    <li className="moreList"><a href={item.more.url[idx]} style={{textDecoration: 'none', color: 'white'}}>{a}</a></li>
-                  )}
-                  </ul>
-                }
-              </li>
-            )
-          })}
-        </ul>
-        <Button>Sign Up</Button>
-      </nav>
+      <div className="wrap_nav">
+        <nav className={this.state.scrolled ? 'NavbarItems active' : 'NavbarItems'}>
+          <a href="/"><h1 className="navbar-logo"><i className="fab fa-react"></i>Jariyo</h1></a>
+          <div className="menu-icon" onClick={this.handleClick}>
+            <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index} onMouseOut={this.moreView2}>
+                  {item.title == '더 보기'
+                  ? <a className={item.cName} href={item.url} onMouseOver={this.moreView}>
+                      {item.title}
+                    </a>
+                  : <a className={item.cName} href={item.url}>
+                      {item.title}
+                    </a>
+                  }
+                  {item.title == '더 보기' &&
+                    <ul className="more" onMouseOver={this.moreView}>
+                    {item.more.title.map((a, idx) =>
+                      <li className="moreList"><a href={item.more.url[idx]} style={{textDecoration: 'none', color: 'white'}}>{a}</a></li>
+                    )}
+                    </ul>
+                  }
+                </li>
+              )
+            })}
+          </ul>
+          <Button>Sign Up</Button>
+        </nav>
+      </div>
     );
   }
 }
