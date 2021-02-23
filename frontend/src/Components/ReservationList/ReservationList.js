f5e0b8999a7dad3beaf25f0a48de2c1b59ed6e35
ewhdtls0 <46704442+ewhdtls0@users.noreply.github.com>
2021-02-23 15:46:27 +0900

    reservation_list_frontend_done

--- frontend/src/Components/ReservationList/ReservationList.js
+++ frontend/src/Components/ReservationList/ReservationList.js
@@ -18,11 +18,24 @@ class ReservationList extends Component{
                         </ul>
                     </Grid>
                     <Grid className="list_item_wrap" item xs={8}>
+                        {true &&
                         <div className="list_item_none">
                             <h3 style={{textAlign: "center"}}>예약 내역이 없습니다.</h3>
                             <p style={{textAlign: "center", paddingTop: "2%"}}>최저가로 예약 가능한 숙소들을 지금 만나세요!</p>
                             <div style={{textAlign: "center", paddingTop: "3%"}}><a href="/reservation/호텔/헤네시스"><button className="go_reservation_btn">예약 하러가기</button></a></div>
+                        </div>}
+
+                        {false &&
+                        <div>
+                            <h3 className="item_title">예약 내역</h3>
+                            <div className="item_list_wrap">
+                                <a href="">
+                                    <img className="items" alt="no image" src="/static/img/hotel_1.jpg" width="100%" height="250px"></img>
+                                    <button className="cancle_reservation_btn">예약 취소</button>
+                                </a>
+                            </div>
                         </div>
+                        }
                     </Grid>
                 </Grid>
                 <Grid item xs={2}></Grid>
